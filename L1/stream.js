2f9a11b868047b581cd15780021f3a6a68be6c97
srivijaykalki <poosarla.s@northeastern.edu>
2023-11-10 17:29:32 -0500

    streams and buffers  - piping

--- L1/stream.js
+++ L1/stream.js
@@ -1,9 +1,14 @@
 const fs = require("fs");
 let readStream = fs.createReadStream("../docs/blog3.txt", { encoding: "utf8" });
 let writeStream = fs.createWriteStream("../docs/blog4.txt");
-readStream.on("data", (chunk) => {
-  console.log("new Stream");
-  console.log(chunk);
-  writeStream.write("\n   new chunck  \n");
-  writeStream.write(chunk);
-});
+// readStream.on("data", (chunk) => {
+//   console.log("new Stream");
+//   console.log(chunk);
+//   writeStream.write("\n   new chunck  \n");
+//   writeStream.write(chunk);
+// });
+
+// piping
+// this is the same implementation as the above method
+// copies all the data from the readStream and puts in the write stream
+readStream.pipe(writeStream);
