e30d4e05be8d03cfbc49af41f438fcfad6eee5a8
poosarla-sri-vijay-kalki <srivijaykalki@gmail.com>
2023-11-10 18:38:10 -0500

    create a server and listenn  to the request

---
+++ L2- CLIENTS&SERVERS/server.js
@@ -0,0 +1,10 @@
+const http = require("http");
+//  in node js we need to create a server unlike in other tools like in apache.
+const server = http.createServer(function (req, res) {
+  console.log("request made");
+});
+// to start listing to request we need to call server.listen()
+// here the function will be called at the start of the listening
+server.listen(3000, "localhost", () => {
+  console.log("listening started");
+});
