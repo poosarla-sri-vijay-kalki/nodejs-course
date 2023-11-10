509c34b1da9d07284625cb522226eb4f65c0a94e
poosarla-sri-vijay-kalki <srivijaykalki@gmail.com>
2023-11-10 18:51:22 -0500

    send respose on request

--- L2- CLIENTS&SERVERS/server.js
+++ L2- CLIENTS&SERVERS/server.js
@@ -2,6 +2,9 @@ const http = require("http");
 //  in node js we need to create a server unlike in other tools like in apache.
 const server = http.createServer(function (req, res) {
   console.log("request made");
+  res.setHeader("Content-Type", "text/plain");
+  res.write("hello vijay");
+  res.end();
 });
 // to start listing to request we need to call server.listen()
 // here the function will be called at the start of the listening
