5553fd37328d3cc2130cfd162712578eeaf79f98
poosarla-sri-vijay-kalki <srivijaykalki@gmail.com>
2023-11-10 20:23:32 -0500

    add status code

--- L2- CLIENTS&SERVERS/server.js
+++ L2- CLIENTS&SERVERS/server.js
@@ -7,12 +7,15 @@ const server = http.createServer(function (req, res) {
   switch (req.url) {
     case "/":
       path += "index.html";
+      res.statusCode = 200;
       break;
     case "/about":
       path += "about.html";
+      res.statusCode = 200;
       break;
     default:
       path += "404.html";
+      res.statusCode = 404;
       break;
   }
   res.setHeader("Content-Type", "text/html");
