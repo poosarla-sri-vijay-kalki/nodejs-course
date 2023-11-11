0483feea19beb9b76e3caa517b1c6029efbcafc7
poosarla-sri-vijay-kalki <srivijaykalki@gmail.com>
2023-11-10 19:07:40 -0500

    send the response from a html file

--- L2- CLIENTS&SERVERS/server.js
+++ L2- CLIENTS&SERVERS/server.js
@@ -1,15 +1,28 @@
 const http = require("http");
+const fs = require("fs");
 //  in node js we need to create a server unlike in other tools like in apache.
 const server = http.createServer(function (req, res) {
   console.log("request made");
   res.setHeader("Content-Type", "text/html");
 
   //    if we add head tag here it's gonna replace the default one.
-  res.write(`
-  <p> Hello vijay </p>
-  `);
-  res.write("<p> lol</p>");
-  res.end();
+  //   res.write(`
+  //   <p> Hello vijay </p>
+  //   `);
+  //   res.write("<p> lol</p>");
+  //   res.end();
+
+  //   send htm file by reading
+  fs.readFile("./views/index.html", (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      //   res.write(data);
+      //   res.end();
+      res.end(data);
+    }
+  });
 });
 // to start listing to request we need to call server.listen()
 // here the function will be called at the start of the listening
